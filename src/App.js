85b329096d61e141525c5c391df42ad13071aaf8
WahyuAndikaRahadi <wahyuandikarahadi19@gmail.com>
2024-01-05 09:01:08 +0700

    Perubahan

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import { useState } from "react";
 import Countdown from "react-countdown";
 
 function App() {
-  const [newYearMessege, setNewYearMessege] = useState(["Sedang Menunggu","Tahun🎆🎇","Baru🧨🎈","2024🥹👌","Cihuyyy😁😎","Freya Jangan Grad di 2024😥😌"])
+  const [newYearMessege, setNewYearMessege] = useState(["Sedang Menunggu","Hari🎆🎇","Ulang Tahun🧨🎈","Untuk Diri Gw🥹👌","Cihuyyy😁😎","Mantap Gess🥳"])
   const particlesInitial = async(engine) => {
     await loadFireworksPreset(engine)
   }
   function timeGone() {
-    const newYearDate = new Date("January 1, 2024 00:00:00").getTime()
+    const newYearDate = new Date("January 19, 2024 00:00:00").getTime()
     const nowDate = new Date().getTime()
     const remainingTime = newYearDate - nowDate
     return remainingTime
@@ -23,11 +23,11 @@ function App() {
       />
       <div className="flex flex-col justify-center items-center min-h-screen gap-4">
         <span className="text-white text-4xl font-bold z-50 px-4">
-          <Typewriter words={newYearMessege} loop={false} cursorStyle={"👋"} typeSpeed={10} cursor/>
+          <Typewriter words={newYearMessege} loop={false} cursorStyle={"🥳"} typeSpeed={10} cursor/>
         </span>
         <div className="z-50 text-white font-bold text-2xl">
           <Countdown date={Date.now() + timeGone()} onComplete={() => setNewYearMessege([
-            "Selamat","Tahun","Baru","2024"
+            "Selamat","Ulang","Tahun","Untuk Diri Gw sendiri","Semoga rezeki nya lancar dan panjang umur😁","Aaminn😊"
           ])}/>
         </div>
       </div>
